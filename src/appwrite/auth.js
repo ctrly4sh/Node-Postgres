020ab8fe00d2b2e6d62732092700f29944574a1d
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-06-24 21:41:41 +0530

    appwrite auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,39 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+    //create account
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                
+            } else {
+                return userAccount;
+            }
+
+        } catch (exe) {
+            throw exe;
+        }
+    }
+
+    async login({email, password}){
+
+    }
+
+}
+
+const authService = new AuthService();
+export default authService;
