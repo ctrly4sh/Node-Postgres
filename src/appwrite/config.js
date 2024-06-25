28bbaed071c854c7458c8730fcc8f2a21099c181
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-06-25 18:09:25 +0530

    POST : create , delete , update

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,16 +1,51 @@
 import config from "../config/config";
+import { Client, ID, Databases, Account, Storage, Query } from "appwrite";
 
-import { Client , ID , Databases , Account , Storage , Query } from "appwrite";
-
-export class Service{
+export class Service {
     client = new Client();
     databases;
     bucket;
-    
-    constructor(){
+
+    constructor() {
         this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    //create post
+    async createPost({ title, slug, content, featuredImage, Status, userID }) {
+        try {
+            return this.databases.createDocument(config.appwriteDatabaseId, config.appwriteCollectionId, documentId, slug, {
+                title,
+                content,
+                featuredImage,
+                Status,
+            }
+            )
+        }
+        catch (exe) {
+            console.log(exe);
+        }
+    }
+
+    //update post 
+    async updatePost(slug, { title, featuredImage, status, userID }) {
+        try {
+            return await this.databases.updateDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, {
+                title,
+                featuredImage,
+                status,
+                userID
+            }
+            )
+        } catch (exe) {
+            console.log(exe);
+        }
     }
 }
 
-const  service = new Service()
+const service = new Service()
 export default service
