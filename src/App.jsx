efe5f10638d753e5c68305f70e31e826a20befb5
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-06-30 15:05:44 +0530

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import "./App.css"
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import {login , logout} from "./store/authSlice"
+import authService from './appwrite/auth';
+import "./App.css";
 
 const App = () => {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        logout(userData)
+      }
+    })
+    .catch((error) => {console.log(error);})
+    .finally()
+  }, [])
+
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
 
   return (
     <div>Blog app with appwrite</div>
